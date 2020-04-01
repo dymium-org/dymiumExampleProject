1f8ebc69bc1502079320018879364f385a047db3
amarin <17020181+asiripanich@users.noreply.github.com>
2020-04-01 19:19:38 +1100

    use dymiumCore with the latest github release versuib

--- setup.R
+++ setup.R
@@ -1,3 +1,4 @@
-remotes::install_github("dymium-org/dymiumCore")
+remotes::install_github("dymium-org/dymiumCore",
+                        ref = remotes::github_release())
 pkgs <- c("data.table", "R6", "sf", "rJava", "checkmate", "here", "assertthat", "purrr", "remotes", "fs", "ggplot2", "testthat", "tidyverse", "modules", "StatMatch")
 install.packages(pkgs)
